9c4ca9e30f7dd8461370939accd0fa9dff211e74
Sascha Rissling <sascha.rissling@gmail.com>
2024-08-31 08:08:22 +0200

    prettify calculator

--- src/pages/pace-calculator/index.tsx
+++ src/pages/pace-calculator/index.tsx
@@ -7,18 +7,18 @@ import { Link } from "react-router-dom";
 
 type Competition = "Sprint" | "Olympic" | "Half Ironman" | "Ironman" | "Custom";
 
+type Length = Record<string, number | undefined>;
+
 const lengths = {
-  Sprint: { swim: 0.75, bike: 20, run: 5 },
-  Olympic: { swim: 1.5, bike: 40, run: 10 },
-  "Half Ironman": { swim: 1.9, bike: 90, run: 21.1 },
-  Ironman: { swim: 3.8, bike: 180, run: 42.2 },
+  Sprint: { swim: 750, bike: 20, run: 5 },
+  Olympic: { swim: 1500, bike: 40, run: 10 },
+  "Half Ironman": { swim: 1900, bike: 90, run: 21.1 },
+  Ironman: { swim: 3800, bike: 180, run: 42.2 },
   Custom: { swim: 0, bike: 0, run: 0 },
 };
 
 const PaceCalculator = () => {
-  const [selectedLength, setSelectedLength] = useState<Record<string | number>>(
-    lengths.Ironman,
-  );
+  const [selectedLength, setSelectedLength] = useState<Length>(lengths.Ironman);
   const [swimPace, setSwimPace] = useState(2);
   const [transition1, setTransition1] = useState(5);
   const [bikePace, setBikePace] = useState(30);
@@ -33,24 +33,42 @@ const PaceCalculator = () => {
 
   const totalTime = useMemo(() => {
     const { swim, bike, run } = selectedLength;
-    const swimTime = (swim ?? 0) * 10 * swimPace;
+    const swimTime = ((swim ?? 0) / 100) * swimPace;
     const bikeTime = ((bike ?? 0) / bikePace) * 60;
     const runTime = (run ?? 0) * runPace;
     const totalTime = swimTime + bikeTime + runTime + transition1 + transition2;
     return formatMinutesToHHMMSS(Number(totalTime.toFixed(2)));
   }, [selectedLength, swimPace, transition1, bikePace, transition2, runPace]);
 
-  const compareLengths = (
-    a: Record<string, number>,
-    b: Record<string, number>,
-  ) => a.swim === b.swim && a.bike === b.bike && a.run === b.run;
+  const compareLengths = (a: Length, b: Length, lengthName: string) => {
+    if (lengthName === "Custom") {
+      return !Object.values(lengths).some(
+        (l) => a.swim === l.swim && a.bike === l.bike && a.run === l.run,
+      );
+    }
+    return a.swim === b.swim && a.bike === b.bike && a.run === b.run;
+  };
+
+  const handleLegChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setSelectedLength((prev) => ({
+      ...prev,
+      [name]: value === "" ? undefined : Number(value),
+    }));
+  };
 
   return (
     <Section>
       <Link to={"/"}>Home</Link>
       <h1>Pace Calculator</h1>
-      Swim:{" "}
-      <div>
+
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          margin: "1rem 0 1rem",
+        }}
+      >
         {Object.keys(lengths).map((length) => (
           <label key={length}>
             <input
@@ -59,6 +77,7 @@ const PaceCalculator = () => {
               checked={compareLengths(
                 selectedLength,
                 lengths[length as Competition],
+                length,
               )}
               onChange={handleLengthChange}
             />
@@ -68,67 +87,40 @@ const PaceCalculator = () => {
       </div>
       <div
         style={{
-          display: "grid",
-          gridTemplateColumns: "100px 100px 100px",
+          display: "flex",
           gap: "1rem",
         }}
       >
-        <div>
+        <label>
           <input
             type="number"
-            value={
-              selectedLength.swim === null
-                ? "Swim length (m)"
-                : selectedLength.swim * 1000
-            }
-            onChange={(event) => {
-              setSelectedLength((prev) => ({
-                ...prev,
-                swim:
-                  event.target.value === ""
-                    ? null
-                    : Number(event.target.value) / 1000,
-              }));
-            }}
-          />{" "}
+            name="swim"
+            value={selectedLength.swim}
+            placeholder="Swim length"
+            onChange={handleLegChange}
+          />
           m
-        </div>
-        <div>
+        </label>
+        <label>
           <input
             type="number"
-            value={
-              selectedLength.bike === null
-                ? "Bike length (km)"
-                : selectedLength.bike
-            }
-            onChange={(event) => {
-              setSelectedLength((prev) => ({
-                ...prev,
-                bike:
-                  event.target.value === "" ? null : Number(event.target.value),
-              }));
-            }}
+            name="bike"
+            value={selectedLength.bike}
+            placeholder="Bike length"
+            onChange={handleLegChange}
           />
           km
-        </div>
-        <div>
+        </label>
+        <label>
           <input
             type="number"
-            value={
-              selectedLength.run === null
-                ? "Run length (km)"
-                : selectedLength.run
-            }
-            onChange={(event) => {
-              setSelectedLength((prev) => ({
-                ...prev,
-                run:
-                  event.target.value === "" ? null : Number(event.target.value),
-              }));
-            }}
+            name="run"
+            value={selectedLength.run}
+            placeholder="Run length"
+            onChange={handleLegChange}
           />
           km
-        </div>
+        </label>
       </div>
       <SliderContainer>
         <Discipline
@@ -140,7 +132,7 @@ const PaceCalculator = () => {
           step={0.05}
           unit="min/100m"
           formattedValue={formatMinutesToMMSS(swimPace)}
-          totalTime={(selectedLength.swim ?? 0) * 10 * swimPace}
+          totalTime={((selectedLength.swim ?? 0) / 100) * swimPace}
         />
         <Discipline
           label="Transition 1"
