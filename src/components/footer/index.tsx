6bfea481d3a7cb367887c9ddd94ea52788484571
Sascha Rissling <sascha.rissling@gmail.com>
2024-09-09 10:52:00 +0200

    add socials

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,8 @@
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import instagramSvg from "../../assets/instagram.svg";
+import githubSvg from "../../assets/github.svg";
 
 const Footer = () => {
   const [shouldPlaceAtBottom, setShouldPlaceAtBottom] = useState(false);
@@ -24,7 +26,16 @@ const Footer = () => {
 
   return (
     <FooterComponent shouldPlaceAtBottom={shouldPlaceAtBottom}>
+      <EmptySpace />
       <Link to={"/imprint"}>Imprint</Link>
+      <SocialContainer>
+        <SocialLink href={"https://instagram.com/430legsup"} target="_blank">
+          <SocialImage src={instagramSvg} alt="Instagram" />
+        </SocialLink>
+        <SocialLink href={"https://github.com/sascharissling"} target="_blank">
+          <SocialImage src={githubSvg} alt="Github" />
+        </SocialLink>
+      </SocialContainer>
     </FooterComponent>
   );
 };
@@ -34,9 +45,14 @@ export default Footer;
 const FooterComponent = styled.footer<{ shouldPlaceAtBottom: boolean }>`
   background-color: ${(props) => props.theme.colors.lightgray};
   color: ${(props) => props.theme.colors.secondary};
-  padding: 1rem;
+  padding: 0 1rem;
   text-align: center;
   width: 100%;
+  align-items: center;
+
+  display: grid;
+  grid-template-columns: 1fr 60% 1fr;
+  grid-template-areas: "empty link social";
 
   ${(props) =>
     props.shouldPlaceAtBottom
@@ -47,4 +63,30 @@ const FooterComponent = styled.footer<{ shouldPlaceAtBottom: boolean }>`
           height: 50px;
         `
       : ""};
+`;
+
+const SocialLink = styled.a`
+  width: 1rem;
+  height: 1rem;
+  opacity: 0.5;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const SocialImage = styled.img`
+  width: 1rem;
+  height: 1rem;
+`;
+
+const EmptySpace = styled.div`
+  grid-area: empty;
+`;
+
+const SocialContainer = styled.div`
+  grid-area: social;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.5rem;
 `;
