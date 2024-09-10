64e421d6281e78e8910a430caf9883c32f5a27de
Sascha Rissling <sascha.rissling@gmail.com>
2024-09-10 14:26:53 +0200

    test GA

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,16 +1,33 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
-//@ts-expect-error no types for react-google-analytics
-import useGoogleAnalytics from "react-google-analytics";
+import { CookieConsent, getCookieConsentValue } from "react-cookie-consent";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 const MainLayout = () => {
-  useGoogleAnalytics(process.env.GA_ID);
+  const cookieConsent =
+    getCookieConsentValue("226toolsCookieAccepted") === "true";
+
+  useEffect(() => {
+    ReactGA.initialize(import.meta.env.VITE_GA_ID);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <NavBar />
       <Outlet />
       <Footer />
+      <CookieConsent
+        enableDeclineButton
+        cookieName="226toolsCookieAccepted"
+        onDecline={() => {
+          alert("nay!");
+        }}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </>
   );
 };
