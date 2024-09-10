595571138a847ca4362a7c8585a9d90da42ce1fd
Sascha Rissling <sascha.rissling@gmail.com>
2024-09-10 13:48:34 +0200

    add GA

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
+//@ts-expect-error no types for react-google-analytics
+import useGoogleAnalytics from "react-google-analytics";
 
 const MainLayout = () => {
+  useGoogleAnalytics(process.env.GA_ID);
   return (
     <>
       <NavBar />
