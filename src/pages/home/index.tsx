e9a924b9265fa60dcc2a220074b46838c6a6922b
Sascha Rissling <sascha.rissling@gmail.com>
2024-09-15 08:17:56 +0200

    add meta

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,10 +2,18 @@ import styled from "styled-components";
 import CardGrid from "../../components/card-grid";
 import { homeCards } from "./data/homeCards.ts";
 import Hero from "./components/Hero.tsx";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <main>
+      <Helmet>
+        <meta
+          name="description"
+          content="226Tools.com helps triathletes improve race performance with tools like the Pace Calculator. Coming soon: Nutrition Calculator and Race Maps to enhance your triathlon training and race day success."
+        />
+      </Helmet>
+
       <Hero />
       <Wrapper>
         <CardGrid content={homeCards} />
